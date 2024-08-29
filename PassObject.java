3ed346fb15dab6a7a1744d2a093de583b779cc4a
Nguyễn Văn Binh <23010742@st.phenikaa-uni.edu.vn>
2024-08-29 08:14:51 +0700

    “Aliasing” In Function Calls
    “Aliasing” In Function Calls

---
+++ PassObject.java
@@ -0,0 +1,11 @@
+public class PassObject {
+    static void f(Number m) {
+    m.i = 15;
+    }
+    public static void main(String[] args) {
+    Number n = new Number();
+    n.i = 14;
+    f(n); // what is n.i now?
+    }
+   }
+   
