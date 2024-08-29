a94fb5fd81f3ac87fed9eb407f16a94fd9bb4987
Nguyễn Văn Binh <23010742@st.phenikaa-uni.edu.vn>
2024-08-29 08:11:20 +0700

    Assignment is Tricky Part 1
    Assignment is Tricky Part 1

---
+++ Number.java
@@ -0,0 +1,15 @@
+public class Number {
+    public int i;
+   }
+   piblic class Assignment1 {
+    public static void main(String[] args) {
+    Assignmebt1 n1 = new Assignmebt1();
+    Number  n2 = new Assignmebt1();
+    n1.i = 2;
+    n2.i = 5;
+    n1.i = n2.i;
+    n2.i = 10;// what is n1.i?
+    }
+   }
+   
+   
