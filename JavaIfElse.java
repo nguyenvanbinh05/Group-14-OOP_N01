4e8282149c297f567c03b9aaeba3028c5b637ee9
Nguyễn Văn Binh <23010742@st.phenikaa-uni.edu.vn>
2024-09-05 07:00:30 +0700

    Add files via upload

---
+++ JavaIfElse.java
@@ -0,0 +1,9 @@
+public class JavaIfElse {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 10;
+        if(x >y) {
+  System.out.println("Hello World");
+}
+    }
+}
