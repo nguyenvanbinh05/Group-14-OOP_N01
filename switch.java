e4c843bafc28bdf636b7c172cfb51e296c61047b
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2024-09-05 17:27:18 +0700

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,29 @@
+public class switch {
+    public station void main(String[] arg){
+        int day = 4 ;
+        switch (day) {
+            case 1:
+            System.out.printfln("Monday");
+            break;
+            case 2 :
+            System.out.printfln("Tuesday");
+            break;
+            case 3 :
+            System.out.printfln("Wednesday");
+            break;
+            case 4:
+          System.out.println("Thursday");
+         break;
+         case 5:
+           System.out.println("Friday");
+          break;
+         case 6:
+         System.out.println("Saturday");
+         break;
+         case 7:
+        System.out.println("Sunday");
+        break;
+        }
+    }
+    
+}
