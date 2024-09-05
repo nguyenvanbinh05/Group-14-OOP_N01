e03a759f3608d24fa0e18e342d23c701bcfd210f
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2024-09-05 17:01:46 +0700

    Add files via upload

---
+++ eachfoop.java
@@ -0,0 +1,9 @@
+public class eachfoop{
+    public static void main(String[] args){
+        String[] cars = { " MEC" , "MADA","MADAM", "MAZDA"};
+        for (String i : cars){
+            System.out.println(i);
+        }
+
+    }
+}
