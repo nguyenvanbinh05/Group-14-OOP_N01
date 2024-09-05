773b2e486ac1077eae8224882f5d522d7366b821
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2024-09-05 07:19:04 +0700

    CODE2 JAVA IF...ELSE

---
+++ int time.java
@@ -0,0 +1,9 @@
+int time = 22;
+if (time < 10) {
+  System.out.println("Good morning.");
+} else if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+// Outputs "Good evening."
